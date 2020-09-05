5929314a7656433f4ece102a06070dac548c1ec7
Vadim Kozin <ulin.salma@yandex.ru>
2020-09-05 18:53:48 +0300

    создал класс Poins для модели данных

---
+++ src/model/point.js
@@ -0,0 +1,56 @@
+import Observer from "../utils/observer.js";
+
+export default class Points extends Observer {
+  constructor() {
+    super();
+    this._points = [];
+  }
+
+  set points(points) {
+    this._points = points.slice();
+  }
+
+  get points() {
+    return this._points;
+  }
+
+  update(updateType, update) {
+    const index = this._points.findIndex((point) => point.id === update.id);
+
+    if (index === -1) {
+      throw new Error(`Can't update unexisting task`);
+    }
+
+    this._points = [
+      ...this._points.slice(0, index),
+      update,
+      ...this._points.slice(index + 1)
+    ];
+
+    this._notify(updateType, update);
+  }
+
+  add(updateType, update) {
+    this._points = [
+      update,
+      ...this._points
+    ];
+
+    this._notify(updateType, update);
+  }
+
+  delete(updateType, update) {
+    const index = this._points.findIndex((point) => point.id === update.id);
+
+    if (index === -1) {
+      throw new Error(`Can't delete unexisting task`);
+    }
+
+    this._points = [
+      ...this._points.slice(0, index),
+      ...this._points.slice(index + 1)
+    ];
+
+    this._notify(updateType);
+  }
+}
